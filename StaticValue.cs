35eacc593fcde28fce02b279de076581027514bc
김정수 <kjs20151240@gmail.com>
2024-10-13 18:48:26 +0900

    init | allfilesave

---
+++ StaticValue.cs
@@ -0,0 +1,10 @@
+public static class StaticValue
+{
+    // Player X,Y 좌표를 static으로 정의
+    public static double PlayerX = 0;
+    public static double PlayerY = 380;
+
+    // Door X,Y 좌표를 static으로 정의
+    public static double DoorX = 300;
+    public static double DoorY = 380;
+}
